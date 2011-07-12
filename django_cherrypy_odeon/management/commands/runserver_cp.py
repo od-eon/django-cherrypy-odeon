03e753c353ae527910b298884b2a32b46afb9842
Stefan Talpalaru <stefantalpalaru@yahoo.com>
2011-07-12 16:39:18 +0200

    support staticfiles

--- django_cherrypy_odeon/management/commands/runserver_cp.py
+++ django_cherrypy_odeon/management/commands/runserver_cp.py
@@ -6,6 +6,7 @@ from wsgiserver import CherryPyWSGIServer, WSGIPathInfoDispatcher
 from optparse import OptionParser, make_option
 import os.path, sys
 from django_cherrypy_odeon.management.commands import runcpserver
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 runcpserver.CPSERVER_HELP = runcpserver.CPSERVER_HELP.replace('runcpserver', 'runserver_cp')
 
@@ -26,7 +27,7 @@ def start_server_with_admin(options):
     from django.core.servers.basehttp import AdminMediaHandler
 
     path = django.__path__[0] + '/contrib/admin/media'
-    dispatcher = AdminMediaHandler(WSGIHandler(), path)
+    dispatcher = StaticFilesHandler(AdminMediaHandler(WSGIHandler(), path))
     threads = int(options['threads'])
     SERVER = Server(
         (options['host'], int(options['port'])),
